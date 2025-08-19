8245e791bcf54a29e163f5aceff9573fbdf3f903
Tolulope McSmith <tolulope.mcsmith@gmail.com>
2025-08-19 23:01:30 +0100

    Starting the Admin Panel Code

---
+++ admin/src/App.js
@@ -0,0 +1,26 @@
+// App.js 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Flowers from "./pages/Flowers";
+import AddFlower from "./pages/AddFlower";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <h1 className="app-title">Admin Panel</h1>
+        <nav className="navbar">
+          <Link to="/flowers" className="nav-link">Flowers</Link>
+          <Link to="/add-flower" className="nav-link">Add Flowers</Link>
+        </nav>
+        <hr />
+        <Routes>
+          <Route path="/flowers" element={<Flowers />} />
+          <Route path="/add-flower" element={<AddFlower />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
